050a488e5a0c53def1bdd826c8d7880b8ab4c05b
D Siddharth <53994789+monsieur-here@users.noreply.github.com>
2026-04-30 16:46:00 +0200

    Implemented information Retrieval for RAG and created ollamaService for Ollama Generation with given conversions

--- server/src/routes/trafficRoute.js
+++ server/src/routes/trafficRoute.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const VehicleEvent = require('../models/VehicleEvent');
 
+const { searchTrafficContext } = require('../services/qdrantService');
+const { embedText } = require('../services/embeddingService');
+const { generateResponse } = require('../services/ollamaService');
+
 router.get('/events', async (req, res) => {
     try {
         const events = await VehicleEvent.find()
@@ -21,6 +25,26 @@ router.get('/counts', async (req, res) => {
         res.json(counts);
     } catch (error) {
         res.status(500).json({ error: error.message });
+    }
+});
+
+router.get('/ask', async (req, res) => {
+    try {
+        const { question } = req.query;
+        if (!question) return res.status(400).json({ error: "What is your question?" });
+
+        // 1. Convert question to vector
+        const queryVector = await embedText(question);
+
+        // 2. Fetch relevant context from Qdrant
+        const context = await searchTrafficContext(queryVector);
+
+        // 3. Get answer from Ollama
+        const answer = await generateResponse(context, question);
+
+        res.json({ question, answer });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
 });
 
